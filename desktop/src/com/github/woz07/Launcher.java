854df2e5a6f464bb9fbf52e1ba096c5dcecc9d40
woz07 <105782312+woz07@users.noreply.github.com>
2024-06-09 20:27:24 +0100

    Initial commit

---
+++ desktop/src/com/github/woz07/Launcher.java
@@ -0,0 +1,17 @@
+package com.github.woz07;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.woz07.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class Launcher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(30);
+		config.setTitle("Pang");
+		config.setResizable(false);
+		config.setWindowedMode(1280, 720);
+		new Lwjgl3Application(new Game(), config);
+	}
+}
