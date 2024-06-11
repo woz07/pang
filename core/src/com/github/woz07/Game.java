e51c21dc391bb20c14dbf17973e62a01579d092b
woz07 <105782312+woz07@users.noreply.github.com>
2024-06-11 10:02:20 +0100

    add: rendered player, for testing

--- core/src/com/github/woz07/Game.java
+++ core/src/com/github/woz07/Game.java
@@ -2,31 +2,39 @@ package com.github.woz07;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.woz07.entities.Player;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Player player;
 	
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		player = new Player(32, 32, 50, new Vector2(640, 360), new Sprite(new Texture("player.png")));
+//		player.getSprite().flip(true, true);  Can be used for rotating the player when moved
+		// Todo:
+		//  - Find right scale for player
+		// 	- Work on player movement
+		//  - Use player::getSprite()::flip(bool, bool) for movement turning
+		//  - Create player facing up, for when player shoots it'll change to that image
 	}
 
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		player.getSprite().draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
